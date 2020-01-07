a853b76afc6df996609f711074584a03e8c90f87
NikiforovJacob <nikiforov.jacob@yandex.ru>
2020-01-07 20:51:37 +0700

    add chart view by week

--- server/src/services/TrainingsService.ts
+++ server/src/services/TrainingsService.ts
@@ -1,30 +1,22 @@
 import * as R from 'ramda';
 
 let trainings = {
-  byId: {
-    3: {
-      id: 3,
-      date: '2019-12-02',
-      trainingType: 'Running',
-      distance: '4.3',
-      comment: 'fe',
-    },
-    2: {
-      id: 2,
-      date: '2020-01-01',
-      trainingType: 'Cycling',
-      distance: '10.8',
-      comment: 'fe',
-    },
-    1: {
-      id: 1,
-      date: '2019-12-29',
-      trainingType: 'Swimming',
-      distance: '0.5',
-      comment: 'fe',
-    },
+  byId: { 
+    13: { id: 13, date: '2019-12-12', trainingType: 'Running', distance: '3.3', comment: 'fe' },
+    12: { id: 12, date: '2019-10-02', trainingType: 'Cycling', distance: '20.3', comment: 'fe' },
+    11: { id: 11, date: '2019-11-08', trainingType: 'Cycling', distance: '18.3', comment: 'fe' },
+    10: { id: 10, date: '2019-09-02', trainingType: 'Cycling', distance: '19.3', comment: 'fe' },
+    9: { id: 9, date: '2019-09-12', trainingType: 'Walking', distance: '12.3', comment: 'fe' },
+    8: { id: 8, date: '2019-10-02', trainingType: 'Walking', distance: '8.8', comment: 'fe' },
+    7: { id: 7, date: '2019-11-02', trainingType: 'Walking', distance: '3.6', comment: 'fe' },
+    6: { id: 6, date: '2019-09-05', trainingType: 'Running', distance: '6.3', comment: 'fe' },
+    5: { id: 5, date: '2019-10-24', trainingType: 'Running', distance: '2.8', comment: 'fe' },
+    4: { id: 4, date: '2019-11-11', trainingType: 'Running', distance: '5.0', comment: 'fe' },
+    3: { id: 3, date: '2019-09-02', trainingType: 'Skiing', distance: '10.3', comment: 'fe' },
+    2: { id: 2, date: '2019-10-22', trainingType: 'Skiing', distance: '12.3', comment: 'fe' },
+    1: { id: 1, date: '2019-11-16', trainingType: 'Skiing', distance: '11.3', comment: 'fe' },
   },
-  allIds: [3, 2, 1],
+  allIds: [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
 }
 
 class TrainingsService {
